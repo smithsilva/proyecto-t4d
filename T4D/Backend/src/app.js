7683327672ceb44d9ae43c90b119a3311c964f1d
Juan <juan98vh@gmail.com>
2026-05-21 12:01:16 -0500

    Soluciones del app.jsx y server.jsx

--- T4D/Backend/src/app.js
+++ T4D/Backend/src/app.js
@@ -12,18 +12,4 @@ app.use(express.json());
 app.use("/productos", productosRoutes);
 app.use("/movimientos", movimientosRoutes);
 
-module.exports = app;const express = require("express");
-const cors = require("cors");
-
-const productosRoutes = require("./routes/productos.routes");
-const movimientosRoutes = require("./routes/movimientos.routes");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/productos", productosRoutes);
-app.use("/movimientos", movimientosRoutes);
-
 module.exports = app;
