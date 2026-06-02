c60f85f45a24b55e01606386b7fddb3f93fb08e5
Juan <juan98vh@gmail.com>
2026-06-02 13:40:33 -0500

    certificado, falta terminaerlo

--- T4D/vite.config.js
+++ T4D/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('./certs/localhost.key'),
+      cert: fs.readFileSync('./certs/localhost.crt'),
+    },
+  },
 })
