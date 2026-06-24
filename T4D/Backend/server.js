7db2aba2ff1e1c83488ca763645872757d2a4828
juan98vh-eng <juan98vh@gmail.com>
2026-06-23 20:28:33 -0500

    server.js

--- T4D/Backend/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-console.log("API_KEY:", process.env.API_KEY);
-const app = require("./src/app");
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en ${PORT}`);
-});
